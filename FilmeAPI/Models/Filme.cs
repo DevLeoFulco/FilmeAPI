ca5bab81182c9982aafda4aab942b15ba26f078f
DevLeoFulco <devleofulco@gmail.com>
2024-01-01 22:39:00 -0300

    Adicionado as Anotation Data para inicio de validação dos dados do usuario

--- FilmeAPI/Models/Filme.cs
+++ FilmeAPI/Models/Filme.cs
@@ -1,9 +1,16 @@
-﻿namespace FilmeAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmeAPI.Models;
 
 public class Filme
 {
+    [Required (ErrorMessage ="O título é obrigatorio")]
     public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero é obrigatorio")]
+    [MaxLength (50, ErrorMessage ="O tamanho máximo do gênero nao pode exceder 50 caracteres.")]
     public string Genero  { get; set; }
+    [Required(ErrorMessage = "A duração é obrigatoria")]
+    [Range(60,240, ErrorMessage ="A duração deve ser de no mínimo 60 e máximo 240 minutos.")]
     public int Duracao { get; set; }
 
 }
