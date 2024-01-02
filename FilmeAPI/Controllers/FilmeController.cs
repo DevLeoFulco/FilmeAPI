96a7c02d5524b3fab5deccc0a67185515edd7350
DevLeoFulco <devleofulco@gmail.com>
2024-01-01 22:12:42 -0300

    Adicionar arquivos de projeto.

---
+++ FilmeAPI/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmeAPI.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmeAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}
+ 
